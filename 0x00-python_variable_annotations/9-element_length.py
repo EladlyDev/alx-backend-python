caea130bbedccc4757af8e87dedb75f88d0705cf
Mohamed Eladly <msrtv365@yahoo.com>
2024-06-06 12:15:23 +0300

    Done with task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module to work with typing"""
+from typing import Iterable, Sequence, Tuple, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples where each tuple contains
+    a sequence and its length"""
+    return [(i, len(i)) for i in lst]
