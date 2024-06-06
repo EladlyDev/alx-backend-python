caa4cf47d213a8cb890bfb74c950d8ec1f9250fb
Mohamed Eladly <msrtv365@yahoo.com>
2024-06-06 12:04:22 +0300

    Done with task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """returns a tuple containing the str and the square of the v"""
+    return (k, v**2)
